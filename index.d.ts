a13610229cd0be0b3732ca25cacd1ee344f6f53f
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2024-10-08 13:06:02 +0330

    .

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 import { FC, ReactNode, MutableRefObject } from 'react';
 import { AP_modal } from "./../../npm/aio-popup";
 import { AIODate, DragClass } from './../../npm/aio-utils';
+import 'leaflet/dist/leaflet.css';
 import './index.css';
 type RN = ReactNode;
 declare const AIOInput: FC<AITYPE>;
@@ -639,3 +640,79 @@ type I_AILogin = {
     };
 };
 export declare const AILogin: FC<I_AILogin>;
+type I_pos = [number, number];
+export type I_marker = {
+    pos: [number, number];
+    html?: ReactNode;
+};
+export type I_shapeStyle = {
+    stroke?: {
+        color?: string;
+        width?: number;
+        dash?: string;
+    };
+    fill?: {
+        color?: string;
+        opacity?: number;
+    };
+};
+export type I_circle = {
+    type: 'circle';
+    center: I_pos;
+    radius?: number;
+    style?: I_shapeStyle;
+};
+export type I_rect = {
+    type: 'rect';
+    points: I_pos[];
+    style?: I_shapeStyle;
+};
+export type I_polyline = {
+    type: 'polyline';
+    points: I_pos[];
+    style?: I_shapeStyle;
+};
+export type I_shape = I_circle | I_rect | I_polyline;
+type I_Map = {
+    children?: React.ReactNode;
+    onChange?: (coords: I_pos) => void;
+    zoom?: {
+        value?: number;
+        wheel?: boolean;
+        control?: boolean;
+    };
+    onChangeZoom?: (zoom: number) => void;
+    markers?: I_marker[];
+    value?: I_pos;
+    marker?: ReactNode | false;
+    style?: any;
+    onClick?: () => void;
+    dragging?: boolean;
+    submitText?: string;
+    onSubmit?: (pos: I_pos) => void;
+    shapes?: I_shape[];
+    footer?: ReactNode;
+    layers?: I_layers;
+    getSearchResult?: (searchValue: string) => Promise<I_searchResult[]>;
+    onSearch?: (searchResult: I_searchResult) => void;
+    mapRef?: any;
+};
+export type I_layers = {
+    position: 'topright' | 'topleft';
+    items: I_layerItem[];
+};
+export type I_layerItem = {
+    name: string;
+    markers?: I_marker[];
+    shapes?: I_shape[];
+    active?: boolean;
+};
+export declare const AIMap: FC<I_Map>;
+type I_searchResult = {
+    text: string;
+    value: string;
+    pos: I_pos;
+    subtext?: string;
+    before?: ReactNode;
+    after?: ReactNode;
+};
